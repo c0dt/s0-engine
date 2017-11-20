07f70dddf86a9b95b3bb90dbb40592e54fcbb950
colintrinity <colintrinityls@gmail.com>
2017-11-20 13:52:40 +0900

    remove three.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,28 @@
-var gulp = require('gulp')
-var browserSync = require('browser-sync').create()
-var browserSyncReuseTab = require('browser-sync-reuse-tab')(browserSync)
-var webpack = require('gulp-webpack')
-var path = require('path')
-var runSequence = require('run-sequence')
+let gulp = require('gulp');
+let browserSync = require('browser-sync').create();
+let browserSyncReuseTab = require('browser-sync-reuse-tab')(browserSync);
+let webpack = require('gulp-webpack');
+let path = require('path');
+let runSequence = require('run-sequence');
 
-gulp.task('bundle-debug', function () {
+gulp.task('bundle-debug', function() {
   return gulp.src('src/Main')
-  .pipe(webpack(require('./webpack.config.js')))
-  .pipe(gulp.dest(path.resolve(__dirname, 'build/debug/js/')))
-})
+    .pipe(webpack(require('./webpack.config.js')))
+    .pipe(gulp.dest(path.resolve(__dirname, 'build/debug/js/')));
+});
 
-gulp.task('html-debug', function () {
+gulp.task('html-debug', function() {
   // gulp.src('./index.html')
   // .pipe(gulp.dest('./build/debug/'))
-})
+});
 
-gulp.task('resources-debug', function () {
+gulp.task('resources-debug', function() {
   gulp.src('./resources/**/*')
-  .pipe(gulp.dest('./build/debug/'))
-})
+    .pipe(gulp.dest('./build/debug/'));
+});
 
 // Static server
-gulp.task('browser-sync', function () {
+gulp.task('browser-sync', function() {
   browserSync.init({
     host: process.env.IP || 'localhost',
     port: process.env.PORT || 3000,
@@ -30,14 +30,14 @@ gulp.task('browser-sync', function () {
       baseDir: ['./build/debug/']
     },
     open: false
-  }, browserSyncReuseTab)
+  }, browserSyncReuseTab);
 
-  gulp.watch('./build/debug/**/*').on('change', browserSync.reload)
-})
+  gulp.watch('./build/debug/**/*').on('change', browserSync.reload);
+});
 
-gulp.task('watch-debug', function () {
-  gulp.watch('resources/**/*', ['resources-debug'])
-  gulp.watch('src/**/*', ['bundle-debug'])
-})
+gulp.task('watch-debug', function() {
+  gulp.watch('resources/**/*', ['resources-debug']);
+  gulp.watch('src/**/*', ['bundle-debug']);
+});
 
-gulp.task('default', runSequence('bundle-debug', 'html-debug', 'resources-debug', 'watch-debug', 'browser-sync'))
+gulp.task('default', runSequence('bundle-debug', 'html-debug', 'resources-debug', 'watch-debug', 'browser-sync'));
--- src/main.js
+++ src/main.js
@@ -1,129 +1,35 @@
-import * as THREE from 'three';
-import CANNON from 'cannon';
-import vertexShader from './shaders/sample.vert';
-import fragmentShader from './shaders/sample.frag';
-
 export default class Main {
   constructor() {
-    this._initPhysics();
-    let axis = new THREE.AxisHelper(100);
-    axis.position.set(0, 1, 0);  
-    
-    this.scene = new THREE.Scene();
-    this.scene.add(axis);
-    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
-    this.camera.position.x = 374;
-    this.camera.position.y = 0;
-    this.camera.position.z = 0;
-    this.camera.up = new THREE.Vector3(0, 0, 1);
-    this.camera.lookAt(new THREE.Vector3(0, 0, 0));
-    // this.camera = new THREE.Camera();
-    // this.camera.position.z = 1;
-    this.planeGeometry = new THREE.PlaneBufferGeometry(1200, 1200, 100, 100);
-    this.geometry = new THREE.SphereGeometry(32);
-    // this.material = new THREE.MeshBasicMaterial({ color: 0x0000cc, wireframe: true });
-    this.uniforms = {
-      time: {
-        value: 1.0
-      },
-      resolution: {
-        value: new THREE.Vector2()
-      }
-    };
-
-    // this.material = new THREE.ShaderMaterial({
-    //   uniforms: this.uniforms,
-    //   vertexShader: vertexShader,
-    //   fragmentShader: fragmentShader
-    // });
-    let plane = new THREE.Mesh(this.planeGeometry, new THREE.MeshBasicMaterial({ color: 0x0000cc, wireframe: true }));
-    this.scene.add(plane);
-
-    this.mesh = new THREE.Mesh(this.geometry, new THREE.MeshBasicMaterial({ color: 0xcc0000, wireframe: true }));
-
-    this.scene.add(this.mesh);
-
-    this.renderer = new THREE.WebGLRenderer();
-    this.renderer.setPixelRatio(window.devicePixelRatio);
-    document.body.appendChild(this.renderer.domElement);
-    this.animate();
-
-    this.gui = new dat.GUI();
-    this.gui.add(this.camera.position, 'x', -1000, 1000);
-    this.gui.add(this.camera.position, 'y', -5, 1000);
-    this.gui.add(this.camera.position, 'z', -1000, 1000);
-
-    this.gui.add(this.camera.rotation, 'x', -Math.PI, Math.PI);
-    this.gui.add(this.camera.rotation, 'y', -Math.PI, Math.PI);
-    this.gui.add(this.camera.rotation, 'z', -Math.PI, Math.PI);
-
     this.onWindowResize();
     window.addEventListener('resize', this.onWindowResize.bind(this), false);
-
+    window.requestAnimationFrame(this.animate.bind(this));
   }
 
   _initPhysics(){
-        // Setup our world
-    this.world = new CANNON.World();
-    this.world.gravity.set(0, 0, -9.82); // m/s²
-    // Create a sphere
-    let radius = 32; // m
-    this.sphereBody = new CANNON.Body({
-      mass: 5, // kg
-      position: new CANNON.Vec3(0, 50, 40), // m
-      shape: new CANNON.Sphere(radius)
-    });
-    this.world.addBody(this.sphereBody);
-    
-    // Create a plane
-    this.groundBody = new CANNON.Body({
-      mass: 0 // mass == 0 makes the body static
-    });
-    let groundShape = new CANNON.Plane();
-    this.groundBody.addShape(groundShape);
-    this.world.addBody(this.groundBody);
-    
-    this.fixedTimeStep = 1.0 / 60.0; // seconds
-    this.maxSubSteps = 3;
-    this.lastTime = undefined;
-
-    this.sphereBody.applyLocalImpulse(new CANNON.Vec3(-50, -50, 100), new CANNON.Vec3(100, 100, 100));
   }
 
   onWindowResize(event) {
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-    this.uniforms.resolution.value.x = this.renderer.domElement.width;
-    this.uniforms.resolution.value.y = this.renderer.domElement.height;
+    // this.renderer.setSize(window.innerWidth, window.innerHeight);
+    // this.uniforms.resolution.value.x = this.renderer.domElement.width;
+    // this.uniforms.resolution.value.y = this.renderer.domElement.height;
   }
   animate(time) {
     window.requestAnimationFrame(this.animate.bind(this));
     this.render();
     this.update(time);
-    // this.stats.update()
-    // window.requestAnimationFrame(this.animate.bind(this))
-    // this.mesh.rotation.x += 0.01;
-    // this.mesh.rotation.y += 0.02;
-    this.uniforms.time.value += 0.05;
+    // this.uniforms.time.value += 0.05;
   }
 
   render() {
-    this.uniforms.time.value += 0.05;
-    this.renderer.render(this.scene, this.camera);
+    console.log('render');
+    // this.renderer.render(this.scene, this.camera);
   }
 
   update(time){
-    if (this.lastTime !== undefined){
-      let dt = (dt - this.lastTime) / 1000;
-      this.world.step(this.fixedTimeStep, dt, this.maxSubSteps);
-    }
-    this.mesh.position.x = this.sphereBody.position.x;
-    this.mesh.position.y = this.sphereBody.position.y;
-    this.mesh.position.z = this.sphereBody.position.z;
-    this.mesh.quaternion.x = this.sphereBody.quaternion.x;
-    this.mesh.quaternion.y = this.sphereBody.quaternion.y;
-    this.mesh.quaternion.z = this.sphereBody.quaternion.z;
-    this.mesh.quaternion.w = this.sphereBody.quaternion.w;
-    this.lastTime = time;
+    // if (this.lastTime !== undefined){
+    //   let dt = (dt - this.lastTime) / 1000;
+    //   this.world.step(this.fixedTimeStep, dt, this.maxSubSteps);
+    // }
   }
 }
 
